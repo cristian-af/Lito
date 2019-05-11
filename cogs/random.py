c8a98a63dc04f0042c9daa31e6c0794cc4de04e9
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2019-05-11 12:28:04 +0300

    Removed dankmeme command.

--- cogs/random.py
+++ cogs/random.py
@@ -13,9 +13,6 @@ from discord.ext.commands.cooldowns import BucketType
 class Random(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.reddit = praw.Reddit(client_id=os.getenv('REDDIT_CLIENT_ID'),
-                        client_secret=os.getenv('REDDIT_CLIENT_SECRET'),
-                        user_agent=os.getenv('REDDIT_USER_AGENT'))
 
     def do_dankmeme(self):
         memes_submissions = self.reddit.subreddit('dankmemes').hot()
@@ -59,24 +56,6 @@ class Random(commands.Cog):
                     await ctx.send(f"{resp['joke']}")
         except Exception as e:
             await ctx.send(f"{e}")
-    
-
-    @commands.command()
-    @commands.cooldown(1,5,BucketType.user)
-    @commands.guild_only()
-    async def dankmeme(self, ctx):
-        """Shows a dank meme from r/dankmemes."""
-        if ctx.author.bot:
-            return
-        
-        try:
-            async with ctx.typing():
-                b = await self.bot.loop.run_in_executor(None, self.do_dankmeme)
-                await ctx.send(b)
-        except Exception as e:
-            await ctx.message.add_reaction(ERROR_EMOJI)
-            await ctx.send(f'{e}')
-
 
 def setup(bot):
     bot.add_cog(Random(bot))
