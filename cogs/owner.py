0bd4e50c98d4e9eea7141cdb68ff4ff9331dd790
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2019-05-11 19:55:34 +0300

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -27,22 +27,9 @@ class Owner(commands.Cog):
         self.sessions = set()
         self.blocked = []
 
-    def cleanup_code(self, content):
-        """Automatically removes code blocks from the code."""
-        # remove ```py\n```
-        if content.startswith('```') and content.endswith('```'):
-            return '\n'.join(content.split('\n')[1:-1])
-
-        # remove `foo`
-        return content.strip('` \n')
-
     async def cog_check(self, ctx):
         return await self.bot.is_owner(ctx.author)
 
-    def get_syntax_error(self, e):
-        if e.text is None:
-            return f'```py\n{e.__class__.__name__}: {e}\n```'
-        return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
 
 
 
@@ -106,83 +93,6 @@ class Owner(commands.Cog):
         await ctx.bot.invoke(new_ctx)
         await ctx.message.add_reaction(f"{SUCCESS_EMOJI}")
 
-    @commands.command(hidden=True, name='eval', aliases=['exec'])
-    async def _eval(self, ctx, *, body: str):
-        """Evaluates a code.
-        Credits: R. Danny made by Danny#0007
-        https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L72-L117
-        """
-
-        env = {
-            'bot': self.bot,
-            'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
-            'message': ctx.message,
-            '_': self._last_result
-        }
-
-        env.update(globals())
-
-        body = self.cleanup_code(body)
-        stdout = io.StringIO()
-
-        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-        await ctx.message.add_reaction(LOADING_EMOJI)
-        start = time.perf_counter()
-
-        try:
-            exec(to_compile, env)
-        except Exception as e:
-            end = time.perf_counter()
-            await ctx.message.remove_reaction(LOADING_EMOJI, member=ctx.me)
-            await ctx.message.add_reaction(ERROR_EMOJI)
-            embed = discord.Embed(color=GREEN_EMBED)
-            embed.title = f"Error. <{ERROR_EMOJI}>"
-            embed.description = f'```py\n{e.__class__.__name__}: {e}\n```'
-            embed.timestamp = datetime.datetime.utcnow()
-            return await ctx.author.send(embed=embed)
-
-        func = env['func']
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()
-        except Exception as e:
-            value = stdout.getvalue()
-            end = time.perf_counter()
-            await ctx.message.remove_reaction(LOADING_EMOJI, member=ctx.me)
-            await ctx.message.add_reaction(ERROR_EMOJI)
-            embed = discord.Embed(color=GREEN_EMBED)
-            embed.title = f"Error. <{ERROR_EMOJI}>"
-            embed.description = f'```py\n{value}{traceback.format_exc()}\n```'
-            embed.timestamp = datetime.datetime.utcnow()
-            await ctx.author.send(embed=embed)
-        else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.remove_reaction(LOADING_EMOJI, member=ctx.me)
-                await ctx.message.add_reaction(SUCCESS_EMOJI)
-            except:
-                pass
-
-            if ret is None:
-                if value:
-                    end = time.perf_counter()
-                    embed = discord.Embed(color=GREEN_EMBED)
-                    embed.title = f"Success. <{SUCCESS_EMOJI}>"
-                    embed.description = f'```py\n{value}\n```'
-                    embed.timestamp = datetime.datetime.utcnow()
-                    await ctx.author.send(embed=embed)
-            else:
-                end = time.perf_counter()
-                self._last_result = ret
-                embed = discord.Embed(color=GREEN_EMBED)
-                embed.title = f"Success. <{SUCCESS_EMOJI}>"
-                embed.description = f'```py\n{value}{ret}\n```'
-                embed.timestamp = datetime.datetime.utcnow()
-                await ctx.author.send(embed=embed)
-
     @commands.command(pass_context=True, aliases=["clp"])
     async def cleanup(self, ctx, count: int):
         """Cleans up the bot's messages."""
@@ -194,26 +104,6 @@ class Owner(commands.Cog):
         await asyncio.sleep(1)
         await wait.delete()
         await ctx.send(f"<{SUCCESS_EMOJI}> **Cleared ​`{count}​` messages.**", delete_after=5)
-
-    @commands.command(pass_context=True, hidden=True)
-    async def emojis(self, ctx, *, guildID: int):
-        """Takes information about the emojis from a server."""
-        try:
-            wait = await ctx.send(f"<{LOADING_EMOJI}> **`Wait for result.`**")
-            g = self.bot.get_guild(guildID)
-            g2 = self.bot.get_guild(465180787667369996)
-
-            ch = await g2.create_text_channel(f'{g.name}-emojis')
-
-            for e in g.emojis:
-                await ch.send(f"`{e}` - {e.url}")
-
-            await wait.delete()
-            await ctx.send(f"<{SUCCESS_EMOJI}> **Done, `{len(g.emojis)}` emojis.**")
-        except Exception as e:
-            await wait.delete()
-            await ctx.send(f"<{ERROR_EMOJI}> **The guildID might be invalid, make sure the bot is there.**\n\n```py\n{type(e).__name__}: {str(e)}\n```")
-
 
     @commands.guild_only()
     @commands.group(hidden=True, name="activity")
