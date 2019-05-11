fe240110b0a69c02b46619fda48b43eee8097fc2
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2019-05-11 16:08:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import platform, pkg_resources
 from utils.settings import GREEN_EMBED
 from datetime import datetime
 from discord.ext.commands.cooldowns import BucketType
+from utils.settings import BOT_TOKEN
 
 BOT_PREFIX = "vitotest "
 
