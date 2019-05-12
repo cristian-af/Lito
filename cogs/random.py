5918e9ad7f8c7b2840ad9df298dd2371241f303b
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2019-05-12 16:31:38 +0300

    Update random.py

--- cogs/random.py
+++ cogs/random.py
@@ -54,7 +54,7 @@ class Random(commands.Cog):
     @commands.command()
     @commands.cooldown(1,5,BucketType.user)
     @commands.guild_only()
-    async def userinfo(self, ctx, member: discord.Member):
+    async def userinfo(self, ctx, member: discord.Member or None):
         """Shows information about the user."""
         if ctx.author.bot:
             return
