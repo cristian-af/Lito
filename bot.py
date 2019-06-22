5e9224d4de3acf93369a837067d36fcc5ccbfbc7
Cristy86 <37700326+Cristy86@users.noreply.github.com>
2019-06-22 12:25:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,13 +15,9 @@ from discord.ext.commands.cooldowns import BucketType
 from utils.settings import BOT_TOKEN, BOT_PREFIX
 description = "A bot written in Python running on Android."
 
-
 bot = commands.Bot(description=description, command_prefix=commands.when_mentioned_or(BOT_PREFIX))
 bot.launch_time = datetime.utcnow()
 startup_extensions = ['cogs.owner','cogs.webhook','cogs.random','cogs.eh','jishaku']
-
-
-
 
 @bot.event
 async def on_ready():
@@ -38,48 +34,48 @@ async def on_ready():
     subprocess.run(["figlet","Vito\nAndroid"])
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{BOT_PREFIX}help | {len(bot.users)} users."))
 
-    @bot.command(name='stats')
-    @commands.cooldown(1,5,BucketType.user) 
-    async def _stats(ctx):
-        """Shows the stats about the bot."""
-        if ctx.author.bot:
+@bot.command(name='stats')
+@commands.cooldown(1,5,BucketType.user) 
+async def _stats(ctx):
+    """Shows the stats about the bot."""
+    if ctx.author.bot:
             return
 
-        total = 0
-        file_amount = 0
-        for path, subdirs, files in os.walk('.'):
-            for name in files:
-                if name.endswith('.py'):
-                    file_amount += 1
-                    with codecs.open('./' + str(pathlib.PurePath(path, name)), 'r', 'utf-8') as f:
-                        for i, l in enumerate(f):
-                            if l.strip().startswith('#') or len(l.strip()) is 0:
-                                pass
-                            else:
-                                total += 1
+    total = 0
+    file_amount = 0
+    for path, subdirs, files in os.walk('.'):
+        for name in files:
+            if name.endswith('.py'):
+                file_amount += 1
+                with codecs.open('./' + str(pathlib.PurePath(path, name)), 'r', 'utf-8') as f:
+                    for i, l in enumerate(f):
+                        if l.strip().startswith('#') or len(l.strip()) is 0:
+                            pass
+                        else:
+                            total += 1
 
-        a1 = "``"
-        a2 = "`"
-        f = pyfiglet.Figlet(font='slant')
-        f2 = f.renderText('Vito')
-        delta_uptime = datetime.utcnow() - bot.launch_time
-        hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
-        minutes, seconds = divmod(remainder, 60)
-        days, hours = divmod(hours, 24)
-        embed = discord.Embed(color=GREEN_EMBED)
-        embed.title = "Stats"
-        embed.description = f"{a1}{a2}{f2}{a1}{a2}\nPython Version: {platform.python_version()}\ndiscord.py version: {pkg_resources.get_distribution('discord.py').version}\nUsers: {len(bot.users)}\nPing latency: {round(bot.latency * 1000)}ms\nOwner: {bot.get_user(339752841612623872)}\nUptime: {days}d, {hours}h, {minutes}m, {seconds}s\nServers: {len(bot.guilds)}\nLine count: {total:,} lines and {file_amount:,} files."
-        embed.set_footer(text=f"{bot.user.name}")
-        embed.set_thumbnail(url=bot.user.avatar_url)
-        embed.timestamp = datetime.utcnow()
-        await ctx.send(embed=embed)                                                         
+    a1 = "``"
+    a2 = "`"
+    f = pyfiglet.Figlet(font='slant')
+    f2 = f.renderText('Vito')
+    delta_uptime = datetime.utcnow() - bot.launch_time
+    hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    days, hours = divmod(hours, 24)
+    embed = discord.Embed(color=GREEN_EMBED)
+    embed.title = "Stats"
+    embed.description = f"{a1}{a2}{f2}{a1}{a2}\nPython Version: {platform.python_version()}\ndiscord.py version: {pkg_resources.get_distribution('discord.py').version}\nUsers: {len(bot.users)}\nPing latency: {round(bot.latency * 1000)}ms\nOwner: {bot.get_user(339752841612623872)}\nUptime: {days}d, {hours}h, {minutes}m, {seconds}s\nServers: {len(bot.guilds)}\nLine count: {total:,} lines and {file_amount:,} files."
+    embed.set_footer(text=f"{bot.user.name}")
+    embed.set_thumbnail(url=bot.user.avatar_url)
+    embed.timestamp = datetime.utcnow()
+    await ctx.send(embed=embed)                                                         
                                                         
-    if __name__ == "__main__":
-        for extension in startup_extensions:
-            try:
-                bot.load_extension(extension)
-            except Exception as e:
-                exc = '{}: {}'.format(type(e).__name__, e)
-                print('Failed to load extension {}\n{}'.format(extension, exc))
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
 
 bot.run(BOT_TOKEN)
