ceebb38395335e02994fe8ebd5341ef48ebfe2d6
c_ristian <37700326+Cristy86@users.noreply.github.com>
2020-04-26 15:45:45 +0300

    Update random.py

--- cogs/random.py
+++ cogs/random.py
@@ -59,15 +59,15 @@ class Random(commands.Cog):
     async def userinfo(self, ctx, member: discord.Member):
         """Shows information about the user."""
         status = member.status
-        if member.status is "online":
+        if status is "online":
             status = f"{status} - <:status_online:596576749790429200>"
-        if member.status is "idle":
+        if status is "idle":
             status = f"{status} - <:status_idle:596576773488115722>"
-        if member.status is "dnd":
+        if status is "dnd":
             status = f"{status} -  <:status_dnd:596576774364856321>"
-        if member.status is "offline":
+        if status is "offline":
             status = f"{status} - <:status_offline:596576752013279242> "
-        if member.status is "streaming":
+        if status is "streaming":
             status = f"{status} - <:status_streaming:596576747294818305>"
         
         embed = discord.Embed(color=GREEN_EMBED)
