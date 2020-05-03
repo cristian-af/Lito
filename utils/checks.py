fa0f301a068a71b3ba04175cdc002b8991a3f83b
c_ristian <37700326+Cristy86@users.noreply.github.com>
2020-05-03 17:42:44 +0300

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -7,3 +7,7 @@ from utils.settings import BLUSERS
 
 def is_bot(ctx):
  return ctx.author.bot == False
+
+def is_pop(ctx):
+ return ctx.author.id =! 393322896568614912
+ raise commands.CheckFailure("You are currently blacklisted from ever using commands.")
