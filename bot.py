976d95920c1a3be4ed214c11d38908ebfb004d7f
c_ristian <37700326+Cristy86@users.noreply.github.com>
2020-05-29 12:03:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,8 +65,8 @@ async def _stats(ctx):
     days, hours = divmod(hours, 24)
     embed = discord.Embed(color=GREEN_EMBED)
     embed.title = "Stats"
-    embed.add_field(name=f"Owned by {owner}", value=f"User ID: {owner.id}\nAccount created: {humanize.naturaldate(owner.created_at)}", inline=True)
-    embed.description = f"OS: {test}\nOS kernel: {platform.system()} {platform.release()}\nOS booted since: {datetime.fromtimestamp(psutil.boot_time()).strftime('%Y-%m-%d %H:%M:%S')}\nPython Version: {platform.python_version()}\ndiscord.py version: {pkg_resources.get_distribution('discord.py').version}\n\nUsers: {len(bot.users)}\nPing latency: {round(bot.latency * 1000)}ms\n\nUptime: {days}d, {hours}h, {minutes}m, {seconds}s\nServers: {len(bot.guilds)}\nLine count: {total:,} lines and {file_amount:,} files.\n\nCPU Usage: {psutil.cpu_percent()}%\nVirtual Memory: {humanize.naturalsize(psutil.virtual_memory().used)} ({psutil.virtual_memory().percent}%)\nSwap memory: {humanize.naturalsize(psutil.swap_memory().used)} - ({psutil.swap_memory().percent}%)"
+    embed.add_field(name=f"Owned by {owner}", value=f"User ID: `{owner.id}`\nAccount created: `{humanize.naturaldate(owner.created_at)}`", inline=True)
+    embed.description = f"OS: `{test}`\nOS kernel: `{platform.system()} {platform.release()}`\nOS booted since: `{datetime.fromtimestamp(psutil.boot_time()).strftime('%Y-%m-%d %H:%M:%S')}`\nPython Version: `{platform.python_version()}`\ndiscord.py version: `{pkg_resources.get_distribution('discord.py').version}`\n\nUsers: `{len(bot.users)}`\nPing latency: `{round(bot.latency * 1000)}ms`\n\nUptime: `{days}d, {hours}h, {minutes}m, {seconds}s`\nServers: `{len(bot.guilds)}`\nLine count: `{total:,} lines and {file_amount:,} files.`\n\nCPU Usage: `{psutil.cpu_percent()}%`\nVirtual Memory: `{humanize.naturalsize(psutil.virtual_memory().used)} ({psutil.virtual_memory().percent}%)`\nSwap memory: `{humanize.naturalsize(psutil.swap_memory().used)} - ({psutil.swap_memory().percent}%)`"
     embed.set_footer(text=bot.user.name)
     embed.set_thumbnail(url=bot.user.avatar_url)
     embed.timestamp = datetime.utcnow()
