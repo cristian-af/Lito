a188a8ed425b7fa816179040c43cd141147c2b25
c_ristian <37700326+Cristy86@users.noreply.github.com>
2020-07-16 12:01:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,15 +3,16 @@ from discord.ext import commands
 
 import asyncio
 import os
-import platform, pkg_resources, subprocess, pyfiglet, psutil
+import platform, pkg_resources, subprocess, pyfiglet
 import codecs, humanize
-import pathlib
 
-from utils.settings import GREEN_EMBED, BOT_TOKEN, BOT_PREFIX
+from utils.settings import BOT_TOKEN, BOT_PREFIX
 import utils.checks
 from datetime import datetime
 from discord.ext.commands.cooldowns import BucketType
-description = "I am a personal bot on a 7 android Huawei/android 5 Lenovo that runs on Termux which is a app on android.\nThere is no invite link, so don't bother to search for one, just no. If you want to check the source just use the command [source]."
+import logging
+
+description = "If you want to check the source just use the command [source]."
 
 def get_prefix(bot, message):
     prefixes = ['Lito ', 'lito ', 'LITO ']
@@ -32,39 +33,13 @@ async def on_ready():
     print(f"Python version is {platform.python_version()} and running on {platform.system()}{platform.release()}.")
           
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{BOT_PREFIX}help | {len(bot.users)} users."))
-
-@bot.command(name='stats', aliases=["info"])
-@commands.check(utils.checks.is_bot)
-@commands.cooldown(1,5,BucketType.user) 
-async def _stats(ctx):
-    """Shows the stats about the bot. 5 second cooldown."""
-    rasp_temp = os.popen("vcgencmd measure_temp").readline()
-    total = 0
-    file_amount = 0
-    for path, subdirs, files in os.walk('.'):
-        for name in files:
-            if name.endswith('.py'):
-                file_amount += 1
-                with codecs.open('./' + str(pathlib.PurePath(path, name)), 'r', 'utf-8') as f:
-                    for i, l in enumerate(f):
-                        if l.strip().startswith('#') or len(l.strip()) == 0:
-                            pass
-                        else:
-                            total += 1
+                                                       
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)     
           
-    delta_uptime = datetime.utcnow() - bot.launch_time
-    owner = bot.get_user(339752841612623872)
-    hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
-    minutes, seconds = divmod(remainder, 60)
-    days, hours = divmod(hours, 24)
-    embed = discord.Embed(color=GREEN_EMBED)
-    embed.title = "Stats"
-    embed.description = f"OS kernel: `{platform.system()} {platform.release()}`\nOS booted since: `{datetime.fromtimestamp(psutil.boot_time()).strftime('%Y-%m-%d %H:%M:%S')}`\nPython Version: `{platform.python_version()}`\ndiscord.py version: `{pkg_resources.get_distribution('discord.py').version}`\n\nUsers: `{len(bot.users)}`\nPing latency: `{round(bot.latency * 1000)}ms`\n\nUptime: `{days}d, {hours}h, {minutes}m, {seconds}s`\nServers: `{len(bot.guilds)}`\nLine count: `{total:,} lines and {file_amount:,} files.`\n\nCPU Usage: `{psutil.cpu_percent()}%`\nCPU Temp: `{rasp_temp.replace('temp=','')}`\nVirtual Memory: `{humanize.naturalsize(psutil.virtual_memory().used)} ({psutil.virtual_memory().percent}%)`\nSwap memory: `{humanize.naturalsize(psutil.swap_memory().used)} - ({psutil.swap_memory().percent}%)`"
-    embed.set_footer(text=bot.user.name)
-    embed.set_thumbnail(url=bot.user.avatar_url)
-    embed.timestamp = datetime.utcnow()
-    await ctx.send(embed=embed)                                                         
-     
 if __name__ == "__main__":
     for extension in startup_extensions:
         try:
