b08de54cb05d923045200a41d09de8ccfbe1b3ee
c_ristian <37700326+Cristy86@users.noreply.github.com>
2020-07-16 12:13:36 +0300

    Update random.py

--- cogs/random.py
+++ cogs/random.py
@@ -17,36 +17,36 @@ class Random(commands.Cog):
         self.bot = bot
 
 
-   @commands.command(aliases=["info"])
-   @commands.check(utils.checks.is_bot)
-   @commands.cooldown(1,5,BucketType.user) 
-   async def stats(ctx):
-   """Shows the stats about the bot. 5 second cooldown."""
-    rasp_temp = os.popen("vcgencmd measure_temp").readline()
-    total = 0
-    file_amount = 0
-    for path, subdirs, files in os.walk('.'):
-        for name in files:
-            if name.endswith('.py'):
-                file_amount += 1
-                with codecs.open('./' + str(pathlib.PurePath(path, name)), 'r', 'utf-8') as f:
-                    for i, l in enumerate(f):
-                        if l.strip().startswith('#') or len(l.strip()) == 0:
-                            pass
-                        else:
-                            total += 1
+    @commands.command(aliases=["info"])
+    @commands.check(utils.checks.is_bot)
+    @commands.cooldown(1,5,BucketType.user) 
+    async def stats(ctx):
+    """Shows the stats about the bot. 5 second cooldown."""
+     rasp_temp = os.popen("vcgencmd measure_temp").readline()
+     total = 0
+     file_amount = 0
+     for path, subdirs, files in os.walk('.'):
+         for name in files:
+             if name.endswith('.py'):
+                 file_amount += 1
+                 with codecs.open('./' + str(pathlib.PurePath(path, name)), 'r', 'utf-8') as f:
+                     for i, l in enumerate(f):
+                         if l.strip().startswith('#') or len(l.strip()) == 0:
+                             pass
+                         else:
+                             total += 1
           
-    delta_uptime = datetime.utcnow() - self.bot.launch_time
-    hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
-    minutes, seconds = divmod(remainder, 60)
-    days, hours = divmod(hours, 24)
-    embed = discord.Embed(color=GREEN_EMBED)
-    embed.title = "Stats"
-    embed.description = f"OS kernel: `{platform.system()} {platform.release()}`\nOS booted since: `{datetime.fromtimestamp(psutil.boot_time()).strftime('%Y-%m-%d %H:%M:%S')}`\nPython Version: `{platform.python_version()}`\ndiscord.py version: `{pkg_resources.get_distribution('discord.py').version}`\n\nUsers: `{len(self.bot.users)}`\nPing latency: `{round(self.bot.latency * 1000)}ms`\n\nUptime: `{days}d, {hours}h, {minutes}m, {seconds}s`\nServers: `{len(self.bot.guilds)}`\nLine count: `{total:,} lines and {file_amount:,} files.`\n\nCPU Usage: `{psutil.cpu_percent()}%`\nCPU Temp: `{rasp_temp.replace('temp=','')}`\nVirtual Memory: `{humanize.naturalsize(psutil.virtual_memory().used)} ({psutil.virtual_memory().percent}%)`\nSwap memory: `{humanize.naturalsize(psutil.swap_memory().used)} - ({psutil.swap_memory().percent}%)`"
-    embed.set_footer(text=self.bot.user.name)
-    embed.set_thumbnail(url=self.bot.user.avatar_url)
-    embed.timestamp = datetime.utcnow()
-    await ctx.send(embed=embed)     
+     delta_uptime = datetime.utcnow() - self.bot.launch_time
+     hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
+     minutes, seconds = divmod(remainder, 60)
+     days, hours = divmod(hours, 24)
+     embed = discord.Embed(color=GREEN_EMBED)
+     embed.title = "Stats"
+     embed.description = f"OS kernel: `{platform.system()} {platform.release()}`\nOS booted since: `{datetime.fromtimestamp(psutil.boot_time()).strftime('%Y-%m-%d %H:%M:%S')}`\nPython Version: `{platform.python_version()}`\ndiscord.py version: `{pkg_resources.get_distribution('discord.py').version}`\n\nUsers: `{len(self.bot.users)}`\nPing latency: `{round(self.bot.latency * 1000)}ms`\n\nUptime: `{days}d, {hours}h, {minutes}m, {seconds}s`\nServers: `{len(self.bot.guilds)}`\nLine count: `{total:,} lines and {file_amount:,} files.`\n\nCPU Usage: `{psutil.cpu_percent()}%`\nCPU Temp: `{rasp_temp.replace('temp=','')}`\nVirtual Memory: `{humanize.naturalsize(psutil.virtual_memory().used)} ({psutil.virtual_memory().percent}%)`\nSwap memory: `{humanize.naturalsize(psutil.swap_memory().used)} - ({psutil.swap_memory().percent}%)`"
+     embed.set_footer(text=self.bot.user.name)
+     embed.set_thumbnail(url=self.bot.user.avatar_url)
+     embed.timestamp = datetime.utcnow()
+     await ctx.send(embed=embed)     
         
     @commands.command()
     @commands.cooldown(1,5,BucketType.user)
